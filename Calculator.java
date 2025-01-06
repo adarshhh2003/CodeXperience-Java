e2bccf8ee2435c2fb61222f743240805e8b96ef5
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-01-06 23:31:06 +0530

    Welcome to the Calculator

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Calculator{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+
+ 			System.out.println("\nWelcome to the Calculator");
+		
+		scanner.close();
+	}
+}
+				
+
